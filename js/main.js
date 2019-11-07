5d6d216431c7ad4232272597427940671bd0affe
Devesh Shanbhag <deveshwork@outlook.com>
2019-11-07 15:33:31 +1100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Devesh Shanbhag.", "Academic.", "Professional.", "Skilled."],
       typeSpeed: 100,
       loop: true,
     });
